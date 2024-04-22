10366ac0da51d992f155c22c9a7b0b965d53c7bd
Vlad <vlad.wpg@gmail.com>
2024-04-22 17:41:33 -0400

    Added DB connection

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import json
 import pathlib
 from discord.ext.commands import has_permissions
 import dotenv
+from pymongo import MongoClient
 
 dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
+db_name = str(os.getenv("DB_USERNAME"))
+db_host = str(os.getenv("DB_HOST"))
+db_password = str(os.getenv("DB_PASSWORD"))
+db_appname = str(os.getenv("DB_APPNAME"))
+
+
+def get_database(name, _pass, host, app_name):
+    CONNECTION_STRING = f"mongodb+srv://{name}:{_pass}@{host}.xnsguws.mongodb.net/?retryWrites=true&w=majority&appName={app_name}"
+    client = MongoClient(CONNECTION_STRING)
+    return client['newpBot']
+
 
 bot = discord.Bot()
-
+db = get_database(db_name, db_password, db_host, db_appname)
 
 @bot.event
 async def on_ready():
